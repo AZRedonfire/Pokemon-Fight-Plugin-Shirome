cd887a9fbc6bc5158810ee18a43da25393391160
AZRedonfire <82833437+AZRedonfire@users.noreply.github.com>
2022-10-20 21:09:44 +0200

    Pokemon FIght Plugin V1.0.2
    Ajout de la boucle permettant d'enregistrer les fichiers

--- Pokemon-Fight-Plugin-Shirome-main/PokemonPlugin/src/ch/azredonfire/pokemonfightplugin/PokemonFightListeners.java
+++ Pokemon-Fight-Plugin-Shirome-main/PokemonPlugin/src/ch/azredonfire/pokemonfightplugin/PokemonFightListeners.java
@@ -1,26 +1,31 @@
 package ch.azredonfire.pokemonfightplugin;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
 
-
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
 
 
 public class PokemonFightListeners implements Listener {
 
     int NbOfItemCustom = 1;
 
-    ItemStack RatataFuite = new ItemStack(Material.COBBLESTONE, 1);
-    ItemStack RatataCran = new ItemStack(Material.STONE, 1);
+    ItemStack ratataFuite = new ItemStack(Material.COBBLESTONE, 1);
+    ItemMeta ratataFuiteM = ratataFuite.getItemMeta() ;
+
+    ItemStack ratataCran = new ItemStack(Material.STONE, 1);
+    ItemMeta ratataCranM = ratataCran.getItemMeta() ;
 
 
 
@@ -29,12 +34,14 @@ public class PokemonFightListeners implements Listener {
         Player player = event.getPlayer();
 
         player.getInventory().clear();
-        player.getInventory().addItem(new ItemStack(Material.ARROW));
-        player.getInventory().addItem(RatataFuite);
+        player.getInventory().setItem(5, new ItemStack(Material.COMPASS));
+        for(int i = 0; i <= CustomItemsListFinal.size(); ++i){
 
+        }
+        player.getInventory().addItem(ratataFuite);
+        player.getInventory().addItem(ratataCran);
 
-
-
+        /*
         for(int i=0; i<5; ++i){
 
             ItemStack CurrentItem = new ItemStack((Material.STONE));
@@ -42,7 +49,27 @@ public class PokemonFightListeners implements Listener {
             player.getInventory().addItem(CurrentItem);
 
         }
-        player.updateInventory();
+        player.updateInventory();*/
+    }
+
+    @EventHandler
+    public void OnInteract(PlayerInteractEvent event){
+
+        Player player = event.getPlayer();
+        Action action = event.getAction();
+        ItemStack it = event.getItem();
+
+        if (it != null && it.getType() == Material.COMPASS){
+            if(action == Action.RIGHT_CLICK_AIR | action == Action.RIGHT_CLICK_BLOCK) {
+
+                Inventory inv = Bukkit.createInventory(null, 54, "§7Pokemon Select");
+
+                player.openInventory(inv);
+            }
+        }
+
+
+
     }
 
 }
